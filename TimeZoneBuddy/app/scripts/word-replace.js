ae510e281dd0decce3d2a3b9c070990bcd5b08e0
James Watson <jwatson4115@outlook.com>
2017-01-21 19:13:12 +0000

    Added RegEx for matching common time zone formats (e.g. "4pm EDT") and a replace method.

--- TimeZoneBuddy/app/scripts/word-replace.js
+++ TimeZoneBuddy/app/scripts/word-replace.js
@@ -1,21 +1,31 @@
-﻿var elements = document.getElementsByTagName('*');
+﻿replaceTextInHtml(getTimeZoneRegex(), 'found');
 
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
+/* Replaces all occurences of a piece of text in an html with another */
+function replaceTextInHtml(oldText, newText) {
+    var elements = document.getElementsByTagName('*');
 
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
 
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            var replacedText = text.replace('hello', 'world');
+        for (var j = 0; j < element.childNodes.length; j++) {
+            var node = element.childNodes[j];
 
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
+            if (node.nodeType === 3) {
+                var text = node.nodeValue;
+
+                var replacedText = text
+                    .replace(oldText, newText);
+
+                if (replacedText !== text) {
+                    element.replaceChild(document.createTextNode(replacedText), node);
+                }
             }
         }
-
     }
-
 }
 
+/* Returns a regex that matches many common time zone formats
+ * Examples: "4pm EDT", "12am GMT", "12pm GMT + 1" */ 
+function getTimeZoneRegex() {
+    return /((2[0-4])|(1[0-9])|[1-9])(:([1-6][0-9]|[0][0-9]))?\s*(pm|am)?\s(edt|gmt|utc|est|cest|pst|ist|cst|mst|bst)\s?((\+|\-)\s?((2[0-4])|(1[0-9])|[1-9])?(:([1-6][0-9]|[0][0-9]))?)?/ig;
+}
